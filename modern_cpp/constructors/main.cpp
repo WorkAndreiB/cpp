3943bdc857c7e34b3b1b8f773afe33a2b0db7f13
Andrei B <andrei.baciu94@gmail.com>
2025-12-06 11:57:03 +0000

    Review finding and improvments for constructors practice

--- modern_cpp/constructors/main.cpp
+++ modern_cpp/constructors/main.cpp
@@ -1,69 +1,74 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
-class MyString{
-private:
+class MyString
+{
+   private:
     int size;
     char* data;
-public:
+
+   public:
     MyString() = default;
 
-    MyString(int size, char* str): size(size), data(new char[size]){
+    MyString(int size, char* str) : size(size), data(new char[size])
+    {
         std::cout << "parameter constructor\n";
 
-        for(size_t i=0; i<strlen(str); ++i)
-        {
-            data[i] = str[i];
-        }
-    } 
+        std::memcpy(data, str, std::min(size - 1, static_cast<int>(strlen(str))));
+    }
 
     // copy constructor
-    MyString(const MyString& other): size(other.size)
+    MyString(const MyString& other) : size(other.size), data(nullptr)
     {
         std::cout << "copy constructor\n";
-        data = new char[size];
-        for(int i=0; i<size; ++i)
+        if (other.data)
         {
-            data[i] = other.data[i];
+            data = new char[size];
+            std::memcpy(data, other.data, size);
         }
     }
     // copy operator
     MyString& operator=(const MyString& other)
     {
-        std::cout <<"operator =\n";
+        std::cout << "operator =\n";
         size = other.size;
-        data = new char[size];
-        
-        for(int i = 0; i<size; ++i)
+
+        if (data)
         {
-            data[i]=other.data[i];
+            std::cout << "assigning to an already initialized objects. delete old data to prevent "
+                         "memory leak\n";
+            delete[] data;
         }
+
+        data = new char[size];
+        std::memcpy(data, other.data, size);
+
         return *this;
     }
 
     // move constructor
-    MyString(MyString&& other)
+    MyString(MyString&& other) : size(other.size), data(other.data)
     {
         std::cout << "Move constructor\n";
-        size = other.size;
-        data = other.data;
         other.data = nullptr;
+        other.size = 0;
     }
 
     // move assignment operator
     MyString& operator=(MyString&& other)
     {
         std::cout << "move operator =\n";
-        if(this != &other)
+        if (this != &other)
         {
-            if(data)
+            if (data)
             {
-                delete [] data;
+                delete[] data;
             }
-            else{
+            else
+            {
                 std::cout << "data is not initialized\n";
             }
-            
+
             data = other.data;
             size = other.size;
 
@@ -74,40 +79,71 @@ public:
 
     ~MyString()
     {
-        if(data != nullptr)
+        if (data != nullptr)
         {
-            std::cout <<"destructor: "<< static_cast<void*>(data) << "\n";
-            delete [] data;
+            std::cout << "destructor: " << static_cast<void*>(data) << "\n";
+            delete[] data;
+            data = nullptr;
         }
         else
         {
-            std::cout <<"already deleted\n";
+            std::cout << "already deleted\n";
         }
-      
     }
 
     void print()
     {
-        std::cout<<"data: " << data << ": addr: " << static_cast<void*>(data) <<"\n";
+        if (data)
+        {
+            std::cout << "data: " << data << ": addr: " << static_cast<void*>(data) << "\n";
+        }
+        else
+        {
+            std::cout << "invalid data\n";
+        }
     }
 };
 
+void print(MyString str)
+{
+    str.print();
+}
+
+MyString createString()
+{
+    return MyString(10, "Hello");
+}
+
 int main()
 {
-    MyString str(10, "Salut!");
+    // copy constructor not called because of optimizations (copy ellision)
+    MyString other_str = createString();
+    other_str.print();
 
-    MyString str2(str);
+    {
+        MyString str(10, "Salut!");
+        MyString str2(str);
+        MyString str3 = str2;
 
-    MyString str3;
-    str3 = str2;
+        std::cout << "Send argument to function by value\n";
+        print(str);
 
-    str.print();
+        str.print();
+        str2.print();
+        str3.print();
 
-    str2.print();
-    str3.print();
+        str3 = str;
+        str3.print();
 
-    MyString str4(std::move(str3));
+        MyString str4(std::move(str3));
+        str3.print();
+        str4.print();
 
-    MyString str5{};
-    str5 = std::move(str);
+        MyString str5{};
+        str5 = std::move(str);
+        str.print();
+        str5.print();
+    }
+
+    std::cout << "The end\n";
 }
