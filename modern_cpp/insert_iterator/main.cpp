9d969c00366e6b32a5e5a183c04cf767e1cc482e
Andrei B <andrei.baciu94@gmail.com>
2025-12-07 11:02:59 +0000

    Implement review findings

--- modern_cpp/insert_iterator/main.cpp
+++ modern_cpp/insert_iterator/main.cpp
@@ -1,45 +1,44 @@
-#include <iostream>
-#include <string>
-#include <iterator>
-#include <vector>
 #include <algorithm>
 #include <array>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
 
 /*
  * Call to print_vector can be done without specifying template parameter in some cases
- * When possible, the compiler will deduce the missing template arguments from the function arguments. 
+ * When possible, the compiler will deduce the missing template arguments from the function
+ * arguments.
  */
 template <typename T>
 void print_vector(std::vector<T> vec)
 {
-    for(const auto& el : vec)
+    for (const auto& el : vec)
     {
-        std::cout << el <<" ";
+        std::cout << el << " ";
     }
     std::cout << "\n";
 }
 
-
 void read_data_from_cin_and_push_to_vector()
 {
-    std::cout <<"Enter some words:\n";
+    std::cout << "Enter some words:\n";
 
-    std::istream_iterator<std::string> str_iterator_istream(std::cin); 
+    std::istream_iterator<std::string> str_iterator_istream(std::cin);
     std::istream_iterator<std::string> eof;
 
     std::vector<std::string> words;
     auto it = std::back_inserter(words);
 
-    while(str_iterator_istream != eof)
+    while (str_iterator_istream != eof)
     {
         it = *str_iterator_istream;
         ++str_iterator_istream;
     }
 
-    std::cout<<"Words:\n";
+    std::cout << "Words:\n";
     print_vector(words);
 }
-
 
 void init_vector_with_n_values()
 {
@@ -52,7 +51,12 @@ void init_vector_with_n_values()
 void fill_vector_with_generated_values()
 {
     std::vector<int> vec(10);
-    std::generate(vec.begin(), vec.end(), [n=0]() mutable {++n; return n*n;});
+    std::generate(vec.begin(), vec.end(),
+                  [n = 0]() mutable
+                  {
+                      ++n;
+                      return n * n;
+                  });
 
     print_vector(vec);
 }
@@ -60,7 +64,7 @@ void fill_vector_with_generated_values()
 // copy element from one vector to another
 void copy_element_to_vector()
 {
-    std::vector<int> v_source{1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v_source{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> v_destination(v_source.size());
 
     std::copy(v_source.cbegin(), v_source.cend(), v_destination.begin());
@@ -72,7 +76,7 @@ void copy_element_to_vector()
 // copy element from one vector to another using back_inserter
 void copy_element_to_vector2()
 {
-    std::vector<int> v_source{1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v_source{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> v_destination;
 
     std::copy(v_source.cbegin(), v_source.cend(), std::back_inserter(v_destination));
@@ -81,10 +85,10 @@ void copy_element_to_vector2()
     print_vector(v_destination);
 }
 
-// copy first n element from one vector to another 
+// copy first n element from one vector to another
 void copy_first_n_element_to_vector()
 {
-    std::vector<int> v_source{1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v_source{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> v_destination;
     int num_of_element_to_copy{5};
 
@@ -94,28 +98,29 @@ void copy_first_n_element_to_vector()
     print_vector(v_destination);
 }
 
-// copy elements from one vector to another if they comply to condiftion
+// copy elements from one vector to another if they comply to condition
 void copy_element_if()
 {
-    std::vector<int> v_source{1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v_source{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> v_destination;
 
-    std::copy_if(v_source.cbegin(), v_source.cend(), std::back_inserter(v_destination), [](int el){return el%2 == 0;});
+    std::copy_if(v_source.cbegin(), v_source.cend(), std::back_inserter(v_destination),
+                 [](int el) { return el % 2 == 0; });
 
     print_vector(v_source);
     print_vector(v_destination);
 }
 
-std::array<int,5> get_array()
+std::array<int, 5> get_array()
 {
-    std::array<int,5> arr{1,2,3,4,5};
+    std::array<int, 5> arr{1, 2, 3, 4, 5};
 
     return arr;
 }
 
 std::vector<int> get_vector()
 {
-    std::vector<int> vec{1,2,3,4,5};
+    std::vector<int> vec{1, 2, 3, 4, 5};
 
     return vec;
 }
@@ -132,16 +137,15 @@ int main()
 
     auto arr = get_array();
 
-    for(const auto& el : arr)
+    for (const auto& el : arr)
     {
         std::cout << el << " ";
     }
-    std::cout <<"\n";
-
+    std::cout << "\n";
 
     auto vec = get_vector();
     print_vector(vec);
 
-    std::cout <<"\n";
+    std::cout << "\n";
     return 0;
 }
--- modern_cpp/misc/main.cpp
+++ modern_cpp/misc/main.cpp
@@ -1,77 +1,66 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 void print(const std::vector<int>& vec)
 {
-    for(const auto& el : vec)
+    for (const auto& el : vec)
     {
-        std::cout << el <<" ";
+        std::cout << el << " ";
     }
-    std::cout <<"\n";
+    std::cout << "\n";
 }
 
+// possible memory leak if p is explicitly deleted
 int* f()
 {
-    int * p = new int{20};
+    int* p = new int{20};
     return p;
 }
 
-void print(char* str)
+void print(std::string& str)
 {
-	std::cout<<str<<"\n";
+    str += "!";
+    std::cout << str << "\n";
 }
 
 auto greeter(const std::string& salutation)
 {
-    return [salutation](const std::string& name){
-        return salutation + " " + name;
-    };
+    return [salutation](const std::string& name) { return salutation + " " + name; };
 }
 
 int main()
 {
+    auto add_new_line = []() { std::cout << "\n\n"; };
     std::cout << "Salut modern cpp\n";
 
+    std::cout << "Initializer list vector initialization\n";
+    print(std::vector<int>{10, 20});
+    std::cout << "Fill constructor vector initialization\n";
+    print(std::vector<int>(10, 20));
+    add_new_line();
 
-    // std::vector<int> vec{1,2,3,4,5,6};
+    // testing conversion from const to non const.
+    std::cout << "Convert from const string for string\n";
+    const std::string msg{"Hello World"};
+    print(const_cast<std::string&>(msg));
+    add_new_line();
 
-    // print(vec);
-
-    // const char* msg="Hello World";
-    // print(const_cast<char*>(msg));
-
-    // int* p = f();
-    // std::cout << "*p= " << *p <<"\n";
-
-    // if(p)
-    // {
-    //     std::cout << "delete p... \n";
-    //     delete p;
-    // }
-
-    // std::cout << "*p= " << *p <<"\n";
-
-    // std::string str{"this is my string example"};
-
-    // for(auto it = str.crbegin(); it!=str.crend(); ++it)
-    // {
-    //     std::cout << *it << " ";
-    // }
-    // std::cout<<"\n";
-    
-    // auto it = str.begin();
-    // std::advance(it,2);
-    // if( it!=std::end(str))
-    // {
-    //     std::cout<<*it<<"\n";
-    // }
+    int* p = f();
+    std::cout << "*p= " << *p << "\n";
+    if (p)
+    {
+        std::cout << "delete p... \n";
+        // WARNING: Dereferencing a pointer after it has been deleted causes undefined behavior!
+        delete p;
+    }
+    add_new_line();
 
     auto greet = greeter("Hello");
 
-    std::cout << greet("Gigi") <<"\n";
+    std::cout << greet("Gigi") << "\n";
 
-    std::cout << greeter("Hello")("Mada")<<"\n";
+    std::cout << greeter("Hello")("Mada") << "\n";
 
     return 0;
 }
